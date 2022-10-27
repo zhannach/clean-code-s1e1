48a387cbb9e5d209e77632d171319fe175dbf0ba
Zhanna <zhanna01992@gmail.com>
2022-10-27 13:15:44 +0300

    refactor: add semantic tags according to rule 1.1. html-and-css-extended.md

--- app.js
+++ app.js
@@ -156,7 +156,7 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
+    var editButton=taskListItem.querySelector(".edit");
     var deleteButton=taskListItem.querySelector(".delete");
 
 
